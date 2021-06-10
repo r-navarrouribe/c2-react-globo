1819ed9f02770d1a5b7a23da8c5764eaccf3f3f9
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-10 12:01:48 +0200

    Cambiar posición del globo onmouseover y transición

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  // Inicio declaraciones
   const randomHeight = Math.floor(Math.random() * window.innerHeight);
   const randomWidth = Math.floor(Math.random() * window.innerWidth);
+  const [posicionGlobo, setPosicionGlobo] = useState({
+    top: randomHeight,
+    left: randomWidth,
+  });
+
+  const moverGlobo = () => {
+    setTimeout(() => {
+      const nuevaPosicion = { top: randomHeight, left: randomWidth };
+      setPosicionGlobo(nuevaPosicion);
+    }, 200);
+  };
+
+  // Fin declaraciones
   return (
     <>
       <div className="contenedor-icono">
         <i
+          onMouseOver={moverGlobo}
           className="icono-globo fab fa-fly"
-          style={{ top: randomHeight, left: randomWidth }}
+          style={posicionGlobo}
         ></i>
+        ;
       </div>
     </>
   );
