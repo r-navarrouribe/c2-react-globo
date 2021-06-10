b2bf39f2d868adb825d1bbf9929e8af6725f2e18
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-10 10:54:26 +0200

    Icono + posición aleatoria

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  const randomHeight = Math.floor(Math.random() * window.innerHeight);
+  const randomWidth = Math.floor(Math.random() * window.innerWidth);
+  return (
+    <>
+      <div className="contenedor-icono">
+        <i
+          className="icono-globo fab fa-fly"
+          style={{ top: randomHeight, left: randomWidth }}
+        ></i>
+      </div>
+    </>
+  );
 }
 
 export default App;
